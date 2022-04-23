746bf39ac17fbb57096a0b36c493caa964824318
Ameen Altajer <ameen.altajer@gmail.com>
2022-04-24 02:28:11 +0300

    Implemented a quick goto file/folder functionality

--- Flow.Launcher.Plugin.DropboxFinder/Main.cs
+++ Flow.Launcher.Plugin.DropboxFinder/Main.cs
@@ -24,22 +24,24 @@ namespace Flow.Launcher.Plugin.DropboxFinder
             if (query.Search == "")
                 return new List<Result>();
 
-            Task<List<SearchMatchV2>> matches = Task.Run(() => DropboxManager.GetRelevantItems(query.Search, 3));
+            Task<List<SearchMatchV2>> matches = Task.Run(() => DropboxManager.GetRelevantItems(query.Search, 10));
 
-            if (matches != null)
+            if (matches.Result.Count != 0)
             {
 
                 var resultList = new List<Result>();
 
                 foreach (SearchMatchV2 match in matches.Result)
                 {
-                    
+
 
                     var result = new Result
                     {
                         Title = match.Metadata.AsMetadata.Value.Name,
                         SubTitle = match.Metadata.AsMetadata.Value.PathDisplay,
-                        IcoPath = "app.png"
+                        IcoPath = "app.png",
+                        Action = context => NavigateToFolder(match.Metadata.AsMetadata.Value.PathDisplay)
+
                     };
 
                     resultList.Add(result);
@@ -50,8 +52,21 @@ namespace Flow.Launcher.Plugin.DropboxFinder
                 
             }
 
-            return new List<Result>();
+            return new List<Result>{ new Result {
 
+                Title = "Nothing found..",
+                //SubTitle = "",
+                //IcoPath = "app.png",
+                }
+
+            };
+
+        }
+
+        private bool NavigateToFolder(string subTitle)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @"E:\Dropbox" + subTitle.Replace('/', '\\'));
+            return true;
         }
 
     }
