5f875be5282ad7089f9f6b51824eacc7c104a923
Ameen Altajer <ameen.altajer@gmail.com>
2022-05-01 00:35:57 +0300

    Implemented the settings menu

---
+++ Flow.Launcher.Plugin.DropboxFinder/DropboxFinderSettings.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Flow.Launcher.Plugin;
+using Flow.Launcher.Plugin.DropboxFinder;
+
+namespace Flow.Launcher.Plugin.DropboxFinder
+{
+    /// <summary>
+    /// Interaction logic for DropboxFinderSettings.xaml
+    /// </summary>
+    public partial class DropboxFinderSettings : UserControl
+    {
+        private readonly Settings _settings;
+
+        public DropboxFinderSettings(Settings settings)
+        {
+           
+            InitializeComponent();
+            this._settings = settings;
+        }
+
+        private void DropboxFinderSettings_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            OAuthRefreshKey.Text = _settings.OAuthRefreshToken;
+
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            _settings.OAuthRefreshToken = OAuthRefreshKey.Text;
+            _settings.Save();
+            
+            
+        }
+
+    }
+
+
+}
---
+++ Flow.Launcher.Plugin.DropboxFinder/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Flow.Launcher.Plugin.DropboxFinder
+{
+    public class Settings
+    {
+        internal string SettingsFileLocation;
+
+        public string OAuthRefreshToken { get; set; }
+
+        internal void Save()
+        {
+            File.WriteAllText(SettingsFileLocation, JsonSerializer.Serialize(this));
+        }
+    }
+}
